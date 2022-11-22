bae095fa5d615bd715f64d0deb2693d50092c6d3
yousha <yousha.mahmood@thesaurus.co.jp>
2022-11-22 10:06:46 +0900

    script changed

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,18 @@
-length = 10;
-function func() {
-  console.log(this.length);
+function Human(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
-var obj = {
-  length: 5,
-  thisFunc: function (func) {
-    func();
-    arguments[0]();
-  },
-};
+function Man(name, age) {
+  Human.call(this, name, age);
+}
 
-obj.thisFunc(func, 3);
+Man.prototype = Object.create(Human.prototype);
+// Man.prototype.constructor = Man;
+
+function check() {
+  var obj = new Man("Tommy Tan", 20);
+  console.log(obj.name);
+  console.log(obj instanceof Human);
+}
+check();
