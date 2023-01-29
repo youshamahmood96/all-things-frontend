08cd96bcba0b80da717bc8ca4b32583d98300242
yousha <yousha.mahmood@thesaurus.co.jp>
2023-01-29 12:35:51 +0900

    readability improved

--- javascript/test.js
+++ javascript/test.js
@@ -1,19 +1,21 @@
-console.log(0 == "0");
-console.log(0 === "0");
-console.log(Object.is(0, "0"));
+// This is a JavaScript Quiz from BFE.dev
 
-console.log(0 == 0);
-console.log(0 === 0);
-console.log(Object.is(0, 0));
+// console.log(0 == "0");
+// console.log(0 === "0");
+// console.log(Object.is(0, false));
 
-console.log(0 == -0);
-console.log(0 === -0);
-console.log(Object.is(0, -0));
+// console.log(0 == 0);
+// console.log(0 === 0);
+// console.log(Object.is(0, 0));
+
+// console.log(0 == -0);
+// console.log(0 === -0);
+// console.log(Object.is(0, -0));
 
 console.log(NaN == NaN);
 console.log(NaN === NaN);
 console.log(Object.is(NaN, NaN));
 
-console.log(0 == false);
-console.log(0 === false);
-console.log(Object.is(0, false));
+// console.log(0 == false);
+// console.log(0 === false);
+// console.log(Object.is(0, false));
